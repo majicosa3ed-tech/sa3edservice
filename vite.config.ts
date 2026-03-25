204b353137ed2d80a21c39a4ba12920cac44886b
majicosa3ed-tech <majicosa3ed@gmail.com>
2026-03-25 02:09:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/sa3edservice/',
-  
+  base: '/sa3edservice/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/index.js',
+        assetFileNames: 'assets/index.[ext]',
+      },
+    },
   },
 });
